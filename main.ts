15b83bbbddd63937ac56ee510f758f8ce975e36f
sun-seungwon <bearbear126@naver.com>
2024-11-01 23:56:36 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    OLED.clear()
+})
 let vol = 0
 let tem = 0
 pins.analogWritePin(AnalogPin.P1, 0)
@@ -8,19 +11,19 @@ OLED.init(128, 64)
 basic.forever(function () {
     item.show()
 })
+// 흙에 수분이 없을 경우 물을 달라는 문구를 OLED에 표시
+basic.forever(function () {
+    basic.showNumber(pins.analogReadPin(AnalogReadWritePin.P3))
+    if (pins.analogReadPin(AnalogReadWritePin.P3) < 300) {
+        OLED.writeStringNewLine("\"water please\"")
+    }
+})
 // 만약 식물 주변의 온도가 화씨 68도(섭씨 20도)를 넘으면 모터(선풍기)가 작동한다.
 basic.forever(function () {
     if (tem <= 68) {
         pins.analogWritePin(AnalogPin.P1, 512)
     } else {
         pins.analogWritePin(AnalogPin.P1, 0)
-    }
-})
-// 흙에 수분이 없을 경우 물을 달라는 문구를 OLED에 표시
-basic.forever(function () {
-    basic.showNumber(pins.analogReadPin(AnalogReadWritePin.P3))
-    if (pins.analogReadPin(AnalogReadWritePin.P3) < 300) {
-        OLED.writeStringNewLine("\"water please\"")
     }
 })
 // 외장센서인 온도계가 읽는 값과 화씨로 변환
